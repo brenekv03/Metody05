8581bda7abac1c8f0194165bd5a7fb236e041809
brenekv.03 <brenekv.03@A321-15>
2023-02-02 11:27:11 +0100

    Přidejte soubory projektu.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metody05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<int> list = new List<int>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int n = int.Parse(textBox1.Text);
+            for(int i = 0; i <n; i++)
+            {
+
+            }
+        }
+    }
+}
